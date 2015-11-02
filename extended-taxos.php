058bb78ae349fe11c9a319c99cf828abbb9bd699
John Blackbourn <johnbillion@gmail.com>
2015-11-02 00:36:14 +0000

    Version 1.6.

--- extended-taxos.php
+++ extended-taxos.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name:  Extended Taxonomies
 Description:  Extended custom taxonomies.
-Version:      1.5.4
+Version:      1.6
 Plugin URI:   https://github.com/johnbillion/extended-taxos
 Author:       John Blackbourn
 Author URI:   https://johnblackbourn.com
