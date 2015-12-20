f3d8d8361835b6a7c4424f275f284ff0c7482881
John Blackbourn <johnbillion@gmail.com>
2015-12-20 06:32:12 +0000

    Docs on docs.

--- README.md
+++ README.md
@@ -5,17 +5,34 @@
 
 Extended Taxonomies is a library which provides extended functionality to WordPress custom taxonomies, allowing developers to quickly build custom taxonomies without having to write the same code again and again.
 
-This is a sister library to [Extended CPTs](https://github.com/johnbillion/extended-cpts).
+This library requires its sister library [Extended CPTs](https://github.com/johnbillion/extended-cpts) to be installed too.
 
 ## Improved defaults ##
 
  * Automatically generated labels and term updated messages
  * Public taxonomy with admin UI enabled
 
+## Extended admin features ##
+
+ * Ridiculously easy custom columns on the term listing screen:
+   * Columns available for term meta and callback functions
+   * User capability restrictions
+ * Several custom meta boxes to choose from for the taxonomy's term input on the post editing screen:
+   * 'simple' for a meta box with a simplified list of checkboxes
+   * 'radio' for a meta box with radio inputs
+   * 'dropdown' for a meta box with a dropdown menu
+   * Or a callback function
+ * Add the taxonomy to the 'At a Glance' section on the dashboard
+
+## Extended front-end features ##
+
+ * Automatic integration with the [Rewrite Rule Testing](https://wordpress.org/plugins/rewrite-testing/) plugin
+
 ## Minimum Requirements ##
 
 **PHP:** 5.4  
-**WordPress:** 4.3  
+**WordPress:** 4.4  
+**[Extended CPTs](https://github.com/johnbillion/extended-cpts):** 3.0
 
 ## Usage ##
 
@@ -55,7 +72,9 @@ register_extended_taxonomy( 'story', 'post', array(
 ) );
 ```
 
-Bam, we have a 'Stories' taxonomy attached to the Post post type, with correctly generated labels and term updated messages, radio buttons in place of the standard meta box for this taxonomy on the post editing screen, and a count of the terms in this taxonomy in the 'At a Glance' dashboard widget.
+Bam, we have a 'Stories' taxonomy attached to the Post post type, with correctly generated labels and term updated messages, radio buttons in place of the standard meta box for this taxonomy on the post editing screen, a custom column in the admin area (you need to handle the term meta population yourself), and a count of the terms in this taxonomy in the 'At a Glance' dashboard widget.
+
+The `register_extended_taxonomy()` function is ultimately a wrapper for `register_taxonomy()`, so any of the latter's parameters can be used.
 
 ## License: GPLv2 or later ##
 
--- extended-taxos.php
+++ extended-taxos.php
@@ -20,11 +20,64 @@
  * GNU General Public License for more details.
  */
 
-/**
- * Wrapper function for instantiating a new ExtendedTaxonomy object. This is the only function you need.
- * See the Extended_Taxonomy class for parameters.
- */
 if ( ! function_exists( 'register_extended_taxonomy' ) ) {
+/**
+ * Register an extended custom taxonomy.
+ *
+ * The `$args` parameter accepts all the standard arguments for `register_taxonomy()` in addition to several custom
+ * arguments that provide extended functionality. Some of the default arguments differ from the defaults in
+ * `register_taxonomy()`.
+ *
+ * The `$taxonomy` parameter is used as the taxonomy name and to build the taxonomy labels. This means you can create
+ * a taxonomy with just two parameters and all labels and term updated messages will be generated for you. Example:
+ *
+ *     register_extended_taxonomy( 'location', 'post' );
+ *
+ * The singular name, plural name, and slug are generated from the taxonomy name. These can be overridden with the
+ * `$names` parameter if necessary. Example:
+ *
+ *     register_extended_taxonomy( 'story', 'post' array(), array(
+ *         'plural' => 'Stories',
+ *         'slug'   => 'tales'
+ *     ) );
+ *
+ * @see register_taxonomy() for default arguments.
+ *
+ * @param string       $taxonomy    The taxonomy name.
+ * @param array|string $object_type Name(s) of the object type(s) for the taxonomy.
+ * @param array  $args {
+ *     Optional. The taxonomy arguments.
+ *
+ *     @type string $meta_box         The name of the custom meta box to use on the post editing screen for this
+ *                                    taxonomy. Three custom meta boxes are provided: 'radio' for a meta box with radio
+ *                                    inputs, 'simple' for a meta box with a simplified list of checkboxes, and
+ *                                    'dropdown' for a meta box with a dropdown menu. You can also pass the name of a
+ *                                    callback function, eg my_super_meta_box(), or boolean false to remove the meta
+ *                                    box. Default null, meaning the standard meta box is used.
+ *     @type bool   $checked_ontop    Whether to always show checked terms at the top of the meta box. This allows you
+ *                                    to override WordPress' default behaviour if necessary. Default false if you're
+ *                                    using a custom meta box (see the $meta_box argument), default true otherwise.
+ *     @type bool   $dashboard_glance Whether to show this taxonomy on the 'At a Glance' section of the admin dashboard.
+ *                                    Default false.
+ *     @type array  $admin_cols       Associative array of admin screen columns to show for this taxonomy. See the
+ *                                    `Extended_Taxonomy_Admin::cols()` method for more information.
+ *     @type bool   $exclusive        This parameter isn't feature complete. All it does currently is set the meta box
+ *                                    to the 'radio' meta box, thus meaning any given post can only have one term
+ *                                    associated with it for that taxonomy. 'exclusive' isn't really the right name for
+ *                                    this, as terms aren't exclusive to a post, but rather each post can exclusively
+ *                                    have only one term. It's not feature complete because you can edit a post in
+ *                                    Quick Edit and give it more than one term from the taxonomy.
+ *     @type bool   $allow_hierarchy  All this does currently is disable hierarchy in the taxonomy's rewrite rules.
+ *                                    Default false.
+ * }
+ * @param array  $names {
+ *     Optional. The plural, singular, and slug names.
+ *
+ *     @type string $plural   The plural form of the taxonomy name.
+ *     @type string $singular The singular form of the taxonomy name.
+ *     @type string $slug     The slug used in the term permalinks for this taxonomy.
+ * }
+ */
 function register_extended_taxonomy( $taxonomy, $object_type, array $args = array(), array $names = array() ) {
 
 	$taxo = new Extended_Taxonomy( $taxonomy, $object_type, $args, $names );
@@ -69,44 +122,14 @@ class Extended_Taxonomy {
 	public $args;
 
 	/**
-	 * Register an extended custom taxonomy.
+	 * Class constructor.
 	 *
-	 * The `$args` parameter accepts all the standard arguments for `register_taxonomy()` in addition to several custom
-	 * arguments that provide extended functionality. Some of the default arguments differ from the defaults in
-	 * `register_taxonomy()`.
+	 * @see register_extended_taxonomy()
 	 *
-	 * The `$taxonomy` parameter is used as the taxonomy name and to build the taxonomy labels. This means you can create
-	 * a taxonomy with just two parameters and all labels and term updated messages will be generated for you. Example:
-	 *
-	 *     register_extended_taxonomy( 'location', 'post' );
-	 *
-	 * The singular name, plural name, and slug are generated from the taxonomy name. These can be overridden with the
-	 * `$names` parameter if necessary. Example:
-	 *
-	 *     register_extended_taxonomy( 'story', 'post' array(), array(
-	 *         'plural' => 'Stories',
-	 *         'slug'   => 'tales'
-	 *     ) );
-	 *
-	 * Extra parameters: (@TODO: better docs here)
-	 *
-	 * - exclusive - boolean - This parameter isn't feature complete. All it does currently is set the
-	 * meta box to the 'radio' meta box, thus meaning any given post can only have one term associated
-	 * with it for that taxonomy. I've realised that 'exclusive' isn't the right name for this, as terms
-	 * aren't exclusive to a post, but rather each post can exclusively have only one term. It's not
-	 * feature complete because you can still edit a post in Quick Edit and give it more than one term
-	 * from the taxonomy.
-	 *
-	 * - allow_hierarchy - boolean - All this does currently is disable hierarchy in the taxonomy's
-	 * rewrite rules. Defaults to false.
-	 *
-	 * - admin_cols - array - Associative array of admin screen columns to show for this taxonomy. See
-	 * the `Extended_Taxonomy_Admin::cols()` method for more information.
-	 *
-	 * @param string       $taxonomy    The taxonomy name
-	 * @param array|string $object_type Name(s) of the object type(s) for the taxonomy
-	 * @param array        $args        The taxonomy arguments (optional)
-	 * @param array        $names       An associative array of the plural, singular and slug names (optional)
+	 * @param string       $taxonomy    The taxonomy name.
+	 * @param array|string $object_type Name(s) of the object type(s) for the taxonomy.
+	 * @param array        $args        Optional. The taxonomy arguments.
+	 * @param array        $names       Optional. An associative array of the plural, singular, and slug names.
 	 */
 	public function __construct( $taxonomy, $object_type, array $args = array(), array $names = array() ) {
 
@@ -260,19 +283,6 @@ class Extended_Taxonomy_Admin {
 
 	/**
 	* Class constructor.
-	*
-	* The $args parameter accepts the following arguments:
-	*
-	* - meta_box - string|bool - The name of the custom meta box to use on the post editing screen for
-	* this taxonomy. Three custom meta boxes are provided: 'radio' for a meta box with radio inputs,
-	* 'simple' for a meta box with a simplified list of checkboxes, and 'dropdown' for a meta box with
-	* a dropdown menu. You can also pass the name of a callback function, eg my_super_meta_box(), or
-	* boolean false to remove the meta box. Defaults to null, meaning the standard meta box is used.
-	*
-	* - dashboard_glance - boolean - Whether to show this taxonomy on the 'At a Glance' section of the
-	* WordPress dashboard. Defaults to false.
-	*
-	* @TODO checked_ontop
 	*
 	* @param Extended_Taxonomy $taxo An extended taxonomy object
 	* @param array             $args The admin arguments
